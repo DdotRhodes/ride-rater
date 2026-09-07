b592327898e199c555de2df9671aaec1e76cb1c0
DdotRhodes <d.rhodes@me.com>
2026-09-07 13:14:54 -0700

    Publish sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Offline cache. Park wifi is bad; the app should not care. */
-var CACHE = "ride-rater-v5";
+var CACHE = "ride-rater-v6";
 var ASSETS = [
   "./", "./index.html", "./styles.css", "./app.js", "./data.js",
   "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"
